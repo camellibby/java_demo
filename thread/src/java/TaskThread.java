699166d46ff7c475311ec5e2b754c1a222196bda
luoxinliang <luoxinliang@xjgreat.com>
2022-03-22 10:33:44 +0800

    update thread module

---
+++ thread/src/java/TaskThread.java
@@ -0,0 +1,43 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author luoxinliang
+ */
+public class TaskThread<T> extends Thread {
+
+    private T t;
+
+    public TaskThread(String threadName, T t) {
+        this.setName(threadName);
+        this.t = t;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+
+            try {
+                Method method = t.getClass().getMethod("getUniqueId");
+                int value = (int) method.invoke(t);
+                System.out.println("thread[" + Thread.currentThread().getName() + "] --> uniqueId[" + value + "]");
+
+            } catch (NoSuchMethodException e) {
+                // TODO 暂不处理
+                continue;
+
+            } catch (IllegalAccessException e) {
+                // TODO 暂不处理
+                continue;
+
+            } catch (InvocationTargetException e) {
+                // TODO 暂不处理
+                continue;
+
+            }
+
+
+        }
+    }
+
+}
---
+++ thread/src/java/UniqueThreadIdGenerator.java
@@ -0,0 +1,33 @@
+/**
+ * @author luoxinliang
+ */
+public class UniqueThreadIdGenerator {
+
+    // 线程局部整型变量
+//    private static final ThreadLocal <Integer> uniqueNum =
+//            new ThreadLocal < Integer > () {
+//                @Override protected Integer initialValue() {
+//                    return 0;
+//                }
+//            };
+
+    private static int uniqueNum = 0;
+
+    //变量值
+    public static int getUniqueId() {
+        return ++uniqueNum;
+    }
+
+    public static void main(String[] args) {
+        UniqueThreadIdGenerator uniqueThreadId = new UniqueThreadIdGenerator();
+        // 为每个线程生成一个唯一的局部标识
+        TaskThread t1 = new TaskThread<>("custom-thread-1", uniqueThreadId);
+        TaskThread t2 = new TaskThread<>("custom-thread-2", uniqueThreadId);
+        TaskThread t3 = new TaskThread<>("custom-thread-3", uniqueThreadId);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+}
+
