daa08036e36f827520334fa94186bdee7de2c867
luoxinliang <camelxinliang@sina.com>
2022-05-10 20:34:57 +0800

    Add decimal issue test

---
+++ common/src/main/java/com/camellibby/common/DecimalApp.java
@@ -0,0 +1,10 @@
+package com.camellibby.common;
+
+import java.math.BigDecimal;
+
+public class DecimalApp {
+    public static void main(String[] args) {
+        BigDecimal val = new BigDecimal(2.1);
+        System.out.println(val + "");
+    }
+}
