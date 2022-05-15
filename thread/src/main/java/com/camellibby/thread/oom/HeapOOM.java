d6fd84f8ba2f841409943a080b3d2f75b41ed257
luoxinliang <camelxinliang@sina.com>
2022-05-15 12:55:13 +0800

    Tiny update

--- thread/src/main/java/com/camellibby/thread/oom/HeapOOM.java
+++ thread/src/main/java/com/camellibby/thread/oom/HeapOOM.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * VM Args：-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=heap.hprof
  * @author luoxinliang
  */
 public class HeapOOM {
