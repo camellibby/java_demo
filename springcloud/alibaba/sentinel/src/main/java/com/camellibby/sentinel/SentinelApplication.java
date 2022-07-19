1f8c55175168ca04758e63d3c75e3bfe6c38f52c
luoxinliang <luoxinliang@xjgreat.com>
2022-07-19 23:22:16 +0800

    Add Sentinel foo client

--- springcloud/alibaba/sentinel/src/main/java/com/camellibby/sentinel/SentinelApplication.java
+++ springcloud/alibaba/sentinel/src/main/java/com/camellibby/sentinel/SentinelApplication.java
@@ -1,48 +1,20 @@
 package com.camellibby.sentinel;
 
-import com.alibaba.csp.sentinel.Entry;
-import com.alibaba.csp.sentinel.SphU;
-import com.alibaba.csp.sentinel.annotation.SentinelResource;
-import com.alibaba.csp.sentinel.slots.block.BlockException;
-import com.alibaba.csp.sentinel.slots.block.RuleConstant;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
-import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.csp.sentinel.dashboard.DashboardApplication;
+import com.alibaba.csp.sentinel.init.InitExecutor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@SpringBootApplication
 public class SentinelApplication {
-    public static void main(String[] args) throws InterruptedException {
-        // 配置规则.
-        initFlowRules();
-
-        while (true) {
-            // 1.5.0 版本开始可以直接利用 try-with-resources 特性
-            try (Entry entry = SphU.entry("HelloWorld")) {
-                // 被保护的逻辑
-                System.out.println("hello world");
-            } catch (BlockException ex) {
-                // 处理被流控的逻辑
-                System.out.println("blocked!");
-            }
-            Thread.sleep(100);
-        }
+    public static void main(String[] args) {
+        triggerSentinelInit();
+        SpringApplication.run(DashboardApplication.class, args);
     }
 
-    @SentinelResource("HelloWorld")
-    public void helloWorld() {
-        // 资源中的逻辑
-        System.out.println("hello world");
-    }
-
-    private static void initFlowRules() {
-        List<FlowRule> rules = new ArrayList<>();
-        FlowRule rule = new FlowRule();
-        rule.setResource("HelloWorld");
-        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-        // Set limit QPS to 20.
-        rule.setCount(5);
-        rules.add(rule);
-        FlowRuleManager.loadRules(rules);
+    private static void triggerSentinelInit() {
+        (new Thread(() -> {
+            InitExecutor.doInit();
+        })).start();
     }
 }
