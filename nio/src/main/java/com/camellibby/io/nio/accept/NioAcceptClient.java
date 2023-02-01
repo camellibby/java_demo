100ff18b164ddfa02e981e764175bf523e5747fb
luoxinliang <luoxinliang@xjgreat.com>
2023-02-01 11:25:01 +0800

    tiny update

--- nio/src/main/java/com/camellibby/io/nio/accept/NioAcceptClient.java
+++ nio/src/main/java/com/camellibby/io/nio/accept/NioAcceptClient.java
@@ -1,31 +1,51 @@
 package com.camellibby.io.nio.accept;
 
+import com.camellibby.io.bio.multiple.BioMultipleClient;
+
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NioAcceptClient {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            new Thread(() -> {
-                try {
-                    Socket socket = new Socket("127.0.0.1", 9999);
-                    while (true) {
-                        //向服务器端发送一条消息
-                        socket.getOutputStream().write(LocalDateTime.now().toString().getBytes(StandardCharsets.UTF_8));
-                        socket.getOutputStream().flush();
-                        Thread.sleep(1000);
-                        //读取服务器返回的消息
-                        InputStream inputStream = socket.getInputStream();
-                        byte[] bytes = new byte[1024];
-                        inputStream.read(bytes, 0, bytes.length);
-                        System.out.println("服务器：" + new String(bytes));
-                    }
-                } catch (IOException | InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }).start();
+    private static ExecutorService executorService = Executors.newFixedThreadPool(1000);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        for (int i = 0; i < 1; i++) {
+            Socket socket = new Socket("127.0.0.1", 9999);
+            executorService.submit(new SocketHandler(socket, "thread" + i));
+            System.out.println(i);
+        }
+        executorService.shutdown();
+    }
+
+    public static class SocketHandler implements Runnable {
+
+        private Socket socket;
+        private String name;
+
+        public SocketHandler(Socket socket, String name) {
+            this.socket = socket;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            try {
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                //向服务器端发送一条消息
+                System.out.println("向服务器端发送一条消息");
+                writer.write(name + "\n");
+                writer.flush();
+
+                //读取服务器返回的消息
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                // 阻塞
+                String msg = reader.readLine();
+                System.out.println("服务器：" + msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- nio/src/main/java/com/camellibby/io/nio/accept/NioAcceptServer.java
+++ nio/src/main/java/com/camellibby/io/nio/accept/NioAcceptServer.java
@@ -1,13 +1,18 @@
 package com.camellibby.io.nio.accept;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NioAcceptServer {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     public static void main(String[] args) throws IOException, InterruptedException {
         ServerSocketChannel server = ServerSocketChannel.open();
         server.socket().bind(new InetSocketAddress(9999));
@@ -21,31 +26,38 @@ public class NioAcceptServer {
                 continue;
             }
             System.out.println("客户端:" + socketChannel.getRemoteAddress() + "已连接到服务器");
-            new Thread(() -> {
-                ByteBuffer buffer = ByteBuffer.allocateDirect(4096);
-                try {
-                    while (true) {
-                        if (socketChannel.read(buffer) > 0) {
-                            buffer.flip();
-                            byte[] bytes = new byte[buffer.limit()];
-                            buffer.get(bytes, 0, buffer.limit());
-                            String msg = new String(bytes);
-                            System.out.println("客户端：" + msg);
+            executorService.submit(new SocketHandler(socketChannel));
+        }
+    }
 
-                            buffer.clear();
-                            buffer.put(("你好，" + msg).getBytes(StandardCharsets.UTF_8));
-                            buffer.flip();
-                            while (buffer.hasRemaining()) {
-                                socketChannel.write(buffer);
-                            }
-                            buffer.clear();
+    public static class SocketHandler implements Runnable {
+        private final SocketChannel socketChannel;
 
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
+        public SocketHandler(SocketChannel socketChannel) {
+            this.socketChannel = socketChannel;
+        }
+
+        @Override
+        public void run() {
+            ByteBuffer buffer = ByteBuffer.allocateDirect(4096);
+            try {
+                socketChannel.read(buffer);
+                buffer.flip();
+                byte[] bytes = new byte[buffer.limit()];
+                buffer.get(bytes, 0, buffer.limit());
+                String msg = new String(bytes);
+                System.out.println("客户端：" + msg);
+
+                buffer.clear();
+                buffer.put(("你好，" + msg).getBytes(StandardCharsets.UTF_8));
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    socketChannel.write(buffer);
                 }
-            }).start();
+                buffer.clear();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
